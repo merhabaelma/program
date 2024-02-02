5540f1d6908e711a54393d9d1975b172314659af
merhabaelma <158529856+merhabaelma@users.noreply.github.com>
2024-02-02 00:13:03 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function reply(response) {
+    if (response === 'Yes') {
+        alert('That\'s fantastic! Can\'t wait for our date!');
+    } else {
+        alert('Oh, no worries! If you change your mind, let me know.');
+    }
+}
